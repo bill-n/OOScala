3bd30c2af22f47f7e8789246be2890f7fe597b08
bill <teebill15@gmail.com>
2019-11-25 15:37:50 +0100

    added config files to OOScala

---
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -0,0 +1,38 @@
+package io.turntabl.scalagettingstarted
+
+object MyApp extends App {
+
+    ////    Printing out my name and my hometown
+    //    val fName:String = "Bill Ali"
+    //    val homeTown:String = "Siniko"
+    //    print(s"$fName was born in $homeTown in the Upper East Region of Ghana" )
+
+    //    Writing a method called threeTimes that takes a string and returns the string
+//    def threeTimes(i:String)= i*3
+//    print(threeTimes("hello"))
+//    //even function
+//    def even(i:Int) = if (i % 2==0)
+//
+//    def wordsToLower(words: String*) =words map (_.toLowerCase())
+//    def even2_?(i: Int) = i%2 ==0
+//    def isWeekendDay(day: String) = day match{
+//    case "saturday" | "sunday" => true
+//    case _ => false}
+//   print( isWeekendDay("Monday"))
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
